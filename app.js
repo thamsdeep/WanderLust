5720aacfe85b0e37e177c4da7cad75b69f472665
thamsdeep <135945627+thamsdeep@users.noreply.github.com>
2024-10-22 17:49:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const listingsData = require("./init/data.js"); 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+  
+});
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+
+app.get("/loadListings", async (req, res) => {
+  try {
+    for (let listing of listingsData) {
+      let newListing = new Listing(listing); // Assuming the schema matches the JSON structure
+      await newListing.save(); // Save each listing
+    }
+    res.send("All listings have been successfully saved!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("There was an error saving the listings.");
+  }
+});
+
+//Create Route
+app.post("/listings",wrapAsync(async(req, res, next) => {
+
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+
+}));
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+app.use((err,req,res,next)=>{
+  res.send("somthing went wrong");
+});
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
